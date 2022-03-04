98324d73257f92ee44e581c020749b224b9d01e1
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-03-04 15:25:54 +0100

    test: explicidly use ARMv7

--- speki-ci/pre_build.sh
+++ speki-ci/pre_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Enable buildx and create a builder for the ARMv8 platform
+# Enable buildx and create a builder for the ARMv7 platform
 docker buildx install
-docker buildx create --platform linux/arm/v8 --use
+docker buildx create --platform linux/arm/v7 --use
