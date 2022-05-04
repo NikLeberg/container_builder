fef3b56f0f7e913554595dbdeaae7706cbf24104
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-05-04 09:31:27 +0200

    ci: dont upload sarif files from dockle scanner

--- vhdl_rpn-ci/pre_build.sh
+++ vhdl_rpn-ci/pre_build.sh
@@ -2,3 +2,9 @@
 
 # https://stackoverflow.com/questions/51903877/docker-load-no-space-left-on-device-rhel
 docker system prune -a -f --volumes
+
+# Skip Trivy scan step, image needs too much disk space.
+echo "::set-output name=trivy_skip::skip"
+
+# Suppress Dockle errors that are caused by baseimage fdagosti/quartus_cyclone.
+echo "DOCKLE_IGNORES=DKL-DI-0001,DKL-DI-0005" >> $GITHUB_ENV
