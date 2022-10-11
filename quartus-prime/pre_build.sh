502d9c1f0a423eecbdcb8c8ceab419b6d66eb180
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-10-11 11:43:50 +0200

    quartus-prime: disable trivy

--- quartus-prime/pre_build.sh
+++ quartus-prime/pre_build.sh
@@ -2,3 +2,6 @@
 
 # https://stackoverflow.com/questions/51903877/docker-load-no-space-left-on-device-rhel
 docker system prune -a -f --volumes
+
+# Skip Trivy scan step, image needs too much disk space.
+echo "::set-output name=trivy_skip::skip"
