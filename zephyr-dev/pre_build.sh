681d504f4dcfdc410b1b9bf2f1b4e15b75e9158b
NikLeberg <niklaus.leuenb+github@gmail.com>
2023-02-16 18:13:57 +0100

    zephyr-dev: trivy runs out of disk space, disable

--- zephyr-dev/pre_build.sh
+++ zephyr-dev/pre_build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Skip Trivy scan step, image needs too much disk space.
+echo "trivy_skip=skip" >> $GITHUB_OUTPUT
+
 # Suppress Dockle error that is caused by following suspicious files:
 # - usr/local/lib/python3.10/dist-packages/isort/settings.py
 # - usr/local/lib/python3.10/dist-packages/dill/settings.py
