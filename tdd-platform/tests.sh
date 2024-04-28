f50f09e1a6571324dfc5d8fbfed790d0f904c81c
NikLeberg <niklaus.leuenb+github@gmail.com>
2024-04-28 20:15:47 +0700

    tdd-platform: adapt to new ci system

--- tdd-platform/tests.sh
+++ tdd-platform/tests.sh
@@ -1,40 +1,35 @@
-# Pull in the (probably) just built image, clone the tdd-platform project repo
-# and try to build for each platform.
-FROM ghcr.io/nikleberg/tdd-platform:staging
+#!/bin/sh
 
+# Pull in the just built image, clone the tdd-platform project repo and try to
+# build for each platform.
+docker build -f - . <<EOF
+FROM ghcr.io/nikleberg/tdd-platform:$1-staging
 RUN git clone https://github.com/NikLeberg/tdd-platform.git --recurse-submodules \
     && cd tdd-platform \
     && git reset --hard 45a37f8
-
 SHELL ["/bin/bash", "-c"]
 WORKDIR /tdd-platform
-
 # ESP32
 RUN source platform/esp32/environment.sh \
     && cmake -S . -B build/esp32 -DPLATFORM=esp32 \
     && make -C build/esp32 all
-
 # ESP8266
 RUN source platform/esp8266/environment.sh \
     && cmake -S . -B build/esp8266 -DPLATFORM=esp8266 \
     && make -C build/esp8266 all
-
 # Carme-M4
 RUN source platform/carme-m4/environment.sh \
     && cmake -S . -B build/carme-m4 -DPLATFORM=carme-m4 \
     && make -C build/carme-m4 all
-
 # Linux
 RUN cmake -S . -B build/linux -DPLATFORM=linux \
     && make -C build/linux all
-
 # Fuzzing - Protobuf
 ADD corpus_proto.tar.bz2 build/linux/tests/fuzzing/buggy_api_proto/corpus
 RUN make -C build/linux fuzzing_buggy_api_proto_run
-
 # Fuzzing - GraphFuzz
 ADD corpus_graphfuzz.tar.bz2 build/linux/tests/fuzzing/buggy_api_graphfuzz/corpus
 RUN make -C build/linux fuzzing_buggy_api_graphfuzz_run
-
 # Symbolic execution
 RUN make -C build/linux symbolic_buggy_api_run
+EOF
