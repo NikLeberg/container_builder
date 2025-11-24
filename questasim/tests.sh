e4fd4fe77e286f28e1e8466eb6cd0122c1abed67
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2025-11-24 13:56:55 +0100

    questasim: add GUI tests

--- questasim/tests.sh
+++ questasim/tests.sh
@@ -26,7 +26,7 @@ testQuestaDesign () {
     docker run --name=mac00ababababab --mac-address=00:ab:ab:ab:ab:ab -d alpine tail -f /dev/null
     docker build --network=container:mac00ababababab -f - . <<EOF
 FROM ghcr.io/nikleberg/questasim:$1-staging
-ADD test_design.tar.bz2 /tmp/test_design
+ADD test/design.tar.bz2 /tmp/test_design
 RUN cd /tmp/test_design/geni/modelsim \
     && vsim -c -do ./../scripts/modelsim_compile.tcl \
     && vsim -c -do ./../scripts/modelsim_test.tcl
@@ -34,20 +34,41 @@ EOF
     docker rm --force mac00ababababab
 }
 
+# Run the Vsim GUI and verify that it started.
+testQuestaGUI () {
+    docker run --rm -v "$(pwd)/test:/test" -w /test \
+        --mac-address=00:ab:ab:ab:ab:ab \
+        --entrypoint bash ghcr.io/nikleberg/questasim:$1-staging -c "\
+            set -e; \
+            export DEBIAN_FRONTEND=noninteractive; \
+            apt-get -q -y update; \
+            apt-get -q -y install --no-install-recommends xvfb x11-apps imagemagick; \
+            Xvfb :0 & \
+            DISPLAY=:0 vsim -gui & \
+            sleep 10; \
+            xwd -display :0 -silent -root -out capture.xwd; \
+            convert capture.xwd capture.png; \
+            compare capture.png expected$1.png diff.png"
+}
+
 # Choose test depending on the given tag of the container/image.
 case $1 in
     22.1)
         testQuestaExe "22.1" "Questa  Intel Starter FPGA Edition-64 vsim 2021.2 Simulator 2021.04 Apr 14 2021"
-        testQuestaDesign $1;;
+        testQuestaDesign $1
+        testQuestaGUI $1;;
     23.1)
         testQuestaExe "23.1" "Questa Intel Starter FPGA Edition-64 vsim 2023.3 Simulator 2023.07 Jul 17 2023"
-        testQuestaDesign $1;;
+        testQuestaDesign $1
+        testQuestaGUI $1;;
     24.1)
         testQuestaExe "24.1" "Questa Intel Starter FPGA Edition-64 vsim 2024.3 Simulator 2024.09 Sep 10 2024"
-        testQuestaDesign $1;;
+        testQuestaDesign $1
+        testQuestaGUI $1;;
     25.1)
         testQuestaExe "25.1" "Questa Altera Starter FPGA Edition-64 vsim 2025.2 Simulator 2025.05 May 31 2025"
-        testQuestaDesign $1;;
+        testQuestaDesign $1
+        testQuestaGUI $1;;
     *)
         echo "Unknown image tag to test against. Aborting."
         exit 1;;
